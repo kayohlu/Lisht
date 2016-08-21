d3a1a0cddbb35e226d5e6dc098e16b274e2f0cd8
Karl Grogan <krlgrgn@gmail.com>
2016-08-21 20:15:28 +0100

    Implement a header view for the table. Add detailed text to each cell for the times of the reminder options. Add didSelectReminder callback.

--- Lisht/ReminderViewController.swift
+++ Lisht/ReminderViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
+import Timepiece
 
 class ReminderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  var didSelectReminder: () -> () = {}
   var containerView: UIView!
   var tableView: UITableView!
-
+  var reminderTimes: [String] = [
+    "Later Today",
+    "Tomorrow",
+    "At The Weekend",
+    "A Specific Time"
+  ]
+  
   override func viewDidLoad() {
     self.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
     
     createContainerViewAndLayoutContraints()
     createTableViewAndLayoutContraints()
-    addGestureRecognisers()    
+    addGestureRecognisers()
   }
   
   override func viewWillAppear(animated: Bool) {
@@ -36,17 +44,32 @@ class ReminderViewController: UIViewController, UITableViewDelegate, UITableView
     
     NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Top , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 100.0).active = true
     NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Bottom , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: -100.0).active = true
-    NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Leading , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 40.0).active = true
-    NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Trailing , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: -40.0).active = true
+    NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Leading , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 20.0).active = true
+    NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.Trailing , relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: -20.0).active = true
     
     containerView.translatesAutoresizingMaskIntoConstraints = false
   }
   
   func createTableViewAndLayoutContraints() {
-    tableView = UITableView(frame: containerView.frame)
+    tableView = UITableView(frame: containerView.frame, style: UITableViewStyle.Plain)
     tableView.delegate = self;
     tableView.dataSource = self;
+    tableView.rowHeight = 56.0
+    tableView.scrollEnabled = false
     tableView.reloadData()
+    
+    let tableHeaderView = UIView(frame: CGRectMake(0, 0, containerView.frame.size.width, 40))
+    // WARNING: Figure out why I have to add 15 points to the x coordinate to align the header with the labels in the table view cells.
+    let headerLabel = UILabel(frame: CGRectMake(15, 0, tableHeaderView.frame.size.width, tableHeaderView.frame.size.height))
+    let textLabelFont = UIFont(name: "Roboto-Regular", size: UIFont.preferredFontForTextStyle(UIFontTextStyleBody).pointSize)
+    headerLabel.text = "Remind Me"
+    headerLabel.font = textLabelFont
+    headerLabel.textColor = UIColor.whiteColor()
+    tableHeaderView.addSubview(headerLabel)
+    tableView.tableHeaderView = tableHeaderView
+    
+    tableView.tableHeaderView!.backgroundColor = UIColor(red: 111.0/255.0, green: 115/255.0, blue: 210/255.0, alpha: 1.0)
+    
     containerView.addSubview(tableView)
     
     NSLayoutConstraint(item: tableView, attribute: NSLayoutAttribute.CenterX , relatedBy: .Equal, toItem: containerView, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0).active = true
@@ -61,34 +84,59 @@ class ReminderViewController: UIViewController, UITableViewDelegate, UITableView
   }
   
   func addGestureRecognisers() {
+    let swallowGestureRecogniser: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ReminderViewController.swallowTouch))
+    // WARNING: Why do I have to do this?
+    swallowGestureRecogniser.cancelsTouchesInView = false
+    containerView.addGestureRecognizer(swallowGestureRecogniser)
+    
     let dismissGestureRecogniser: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ReminderViewController.dismissVC))
     self.view.addGestureRecognizer(dismissGestureRecogniser)
     
-    let swallowGestureRecogniser: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ReminderViewController.swallowTouch))
-    containerView.addGestureRecognizer(swallowGestureRecogniser)
   }
   
-  func dismissVC()  {
+  func dismissVC(sender: AnyObject)  {
     self.dismissViewControllerAnimated(true, completion: nil)
   }
   
-  func swallowTouch() {}
+  func swallowTouch(sender: AnyObject) {
+    // Swallowing touches.
+  }
   
-  // MARK tableview delegate methods
+  // MARK: tableview delegate methods
   
   func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 1
   }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 6
+    return reminderTimes.count
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = UITableViewCell()
-    cell.textLabel?.text = indexPath.row.description
+    let dates = [
+      NSDate() + 6.hours, // Later today - 6 hours later
+      NSDate.tomorrow() // Same time tomorrow
+    ]
+    
+    let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "ReminderTableViewCell")
+    
+    let textLabelFont = UIFont(name: "Roboto-Light", size: UIFont.preferredFontForTextStyle(UIFontTextStyleBody).pointSize)
+    cell.textLabel?.font = textLabelFont
+    cell.textLabel?.textColor = UIColor.init(red: 87/255, green: 85/255, blue: 91/255, alpha: 1)
+    cell.textLabel?.text = reminderTimes[indexPath.row]
+    
+    let detailTextLabelFont = UIFont(name: "Roboto-Light", size: UIFont.preferredFontForTextStyle(UIFontTextStyleCaption1).pointSize)
+    cell.detailTextLabel?.font = detailTextLabelFont
+    cell.detailTextLabel?.text = NSDate().stringFromFormat("EEE MMM d, h:mm a")
+    cell.detailTextLabel?.textColor = UIColor.init(red: 87/255, green: 85/255, blue: 91/255, alpha: 0.5)
+    
+    print("Cell recursive description:\n\n\(self.tableView.performSelector(Selector("recursiveDescription")))\n\n")
     
     return cell
   }
-
+  
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    didSelectReminder()
+  }
+  
 }
